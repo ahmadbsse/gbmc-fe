6e83d584e8a5212e8d820116f295b4bba29c5723
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-02-08 02:35:14 +0500

    Implemented contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,45 @@
-import React from "react";
-import { Mail, Phone, MapPin } from "lucide-react";
+import React, { useRef, useState } from "react";
+import { Mail, Phone, MapPin, MailPlus } from "lucide-react";
 import Head from "next/head";
-
+import emailjs from "@emailjs/browser";
 import { Navbar, BaseButton } from "@/components/common";
 import { appData } from "@/constants";
 
+import showToast from "@/utils/toast";
+
 const ContactPage = () => {
+  const [loading, setLoading] = useState(false);
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (
+      process.env.NEXT_PUBLIC_EmailJsServiceId === undefined ||
+      process.env.NEXT_PUBLIC_EmailJsTemplateId === undefined ||
+      process.env.NEXT_PUBLIC_EmailJsPublicKey === undefined
+    ) {
+      showToast("EmailJs Credentials not configured!", "error");
+      return;
+    }
+    setLoading(true);
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EmailJsServiceId,
+        process.env.NEXT_PUBLIC_EmailJsTemplateId,
+        form.current,
+        process.env.NEXT_PUBLIC_EmailJsPublicKey
+      )
+      .then(
+        () => {
+          showToast("Email successfully sent!", "success");
+          setLoading(false);
+          form.current.reset();
+        },
+        (error) => {
+          showToast("Something went wrong!", "error");
+          console.log(`Failed to send email: ${error.text}`);
+        }
+      );
+  };
   return (
     <>
       <Head>
@@ -38,40 +72,51 @@ const ContactPage = () => {
             <div className="grid gap-12 md:grid-cols-2">
               {/* Contact Form */}
               <div className="rounded-lg bg-white p-6 shadow-md">
-                <h2 className="mb-6 text-2xl font-semibold text-slate-800">Send us a message</h2>
-                <form className="space-y-4">
+                <h2 className="mb-6 text-2xl font-semibold text-slate-800">Send us an Email</h2>
+                <form ref={form} onSubmit={sendEmail} className="space-y-4">
                   <div>
                     <label className="mb-1 block text-sm font-medium text-slate-700">Name</label>
                     <input
+                      name="from_name"
                       type="text"
+                      maxLength={100}
+                      required
                       className="w-full rounded-lg border border-slate-300 px-4 py-2 outline-none focus:border-primary focus:border-transparent focus:ring-1 focus:ring-primary"
                     />
                   </div>
                   <div>
-                    <label className="mb-1 block text-sm font-medium text-slate-700">Email</label>
+                    <label className="mb-1 block text-sm font-medium text-slate-700">
+                      Your Email
+                    </label>
                     <input
+                      name="reply_to"
                       type="email"
-                      className="w-full rounded-lg border border-slate-300 px-4 py-2 outline-none focus:border-primary focus:border-transparent focus:ring-1 focus:ring-primary"
-                    />
-                  </div>
-                  <div>
-                    <label className="mb-1 block text-sm font-medium text-slate-700">Subject</label>
-                    <input
-                      type="text"
+                      maxLength={100}
+                      required
                       className="w-full rounded-lg border border-slate-300 px-4 py-2 outline-none focus:border-primary focus:border-transparent focus:ring-1 focus:ring-primary"
                     />
                   </div>
                   <div>
                     <label className="mb-1 block text-sm font-medium text-slate-700">Message</label>
                     <textarea
+                      name="message"
                       rows={4}
+                      maxLength={500}
+                      required
                       className="w-full rounded-lg border border-slate-300 px-4 py-2 outline-none focus:border-primary focus:border-transparent focus:ring-1 focus:ring-primary"
                     />
                   </div>
 
                   <div className="mx-auto mt-4 w-fit">
-                    <BaseButton loading={false} type="submit" handleClick={() => {}}>
-                      Send Message
+                    <BaseButton loading={false} type="submit" disabled={loading}>
+                      {loading ? (
+                        "Loading...."
+                      ) : (
+                        <div className="flex items-center justify-center gap-3">
+                          <span>Send Email</span>
+                          <MailPlus height={20} />
+                        </div>
+                      )}
                     </BaseButton>
                   </div>
                 </form>
--- process.d.ts
+++ process.d.ts
@@ -3,5 +3,8 @@ declare namespace NodeJS {
         JWT_SECRET: string;
         NEXT_PUBLIC_BASE_URL: string;
         NEXT_PUBLIC_API_BASE_URL: string;
+        NEXT_PUBLIC_EmailJsServiceId: string;
+        NEXT_PUBLIC_EmailJsTemplateId: string;
+        NEXT_PUBLIC_EmailJsPublicKey: string;
     }
 }
