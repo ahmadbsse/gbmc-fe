165d94b4179999c6aa2587fedcc95a222545ba67
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-03-02 22:21:11 +0500

    Removed Pre

--- components/user/AllParts.jsx
+++ components/user/AllParts.jsx
@@ -140,7 +140,6 @@ const AllParts = () => {
   };
   return (
     <>
-      {selectedSupplier}
       <div className="mt-5 flex flex-col justify-between lg:flex-row lg:items-center lg:gap-3">
         <div className="mb-6 pr-2 sm:ml-auto sm:px-4 md:pr-0">
           <BaseSearchbar setSearchQuery={setSearchQuery} />
