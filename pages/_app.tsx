fa8323f46ebe1cf046edd2a47424ea45c6d79a77
Muaviya Imran <muaviyaimran1122@gmail.com>
2025-05-05 01:36:54 +0500

    Fixed Build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "@/styles/globals.css";
 import { X } from "lucide-react";
 import { ToastContainer, CloseButtonProps } from "react-toastify";
